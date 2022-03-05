a1f3eb90adb880603c0f16b70e93c22ea0e2fd36
Ian Familara <itoyfamilara@gmail.com>
2022-03-06 01:41:21 +0800

    added index

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ mongoose.connect('mongodb+srv://admin:admin1234@zuitt-bootcamp.2q2jw.mongodb.net
 	{
 		useNewUrlParser: true, 
 		useUnifiedTopology: true
-	}
-);
+	})
 
+// mongoose.connect("mongodb+srv://admin:admin123@cluster0.7iowx.mongodb.net/csp3?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'))
 
-// Server setup
+// Server 
 const app = express();
 
 const corsOptions = {
-	origin: ['http://localhost:3000', 'https://enigmatic-crag-04546.herokuapp.com'],
+	origin: ['http://localhost:3000', 'https://csp3-heroku-deployment.herokuapp.com'],
 	optionsSuccessStatus: 200
 }
 
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-//connect routes
+//connect route
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
 
